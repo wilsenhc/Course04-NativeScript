4f5287ef23f1a4165fe60d9fb154ae69c97bf847
Wilsen Hernández <wilsenh95@gmail.com>
2021-09-04 19:22:42 -0400

    save base url

--- src/app/shared/baseurl.ts
+++ src/app/shared/baseurl.ts
@@ -1,3 +1,4 @@
 // export const baseURL = 'http://127.0.0.1:3000/';
-// export const baseURL = 'https://ngrok.io/';
-export const baseURL = 'http://192.168.88.51:3000/';
+// export const baseURL = 'http://192.168.88.51:3000/';
+export const baseURL = 'http://10.0.2.2:3000/';
+// export const baseURL = 'http://26a6-2a0d-5600-49-3000-00-3ca.ngrok.io/';
